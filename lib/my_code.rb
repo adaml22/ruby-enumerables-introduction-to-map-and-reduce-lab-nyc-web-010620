977fdf37a0ffe7b53b358fb76877691bafa0b0d4
Adam Longman <boardhead89@gmail.com>
2020-01-02 19:15:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,35 @@
 # My Code here....
+def map_to_negativize(array)
+  new_array = map_to_negativize(array)
+  return new_array
+end
+
+def map_to_no_change(array)
+  new_array = map_to_no_change(array)
+  return new_array
+end
+
+def map_to_double(array)
+  new_array = map_to_double(array)
+  return new_array
+end
+
+def map_to_square(array)
+  new_array = map_to_square(array)
+  return new_array
+end
+
+def reduce_to_total(array, starting_point)
+  value = reduce_to_total(array, starting_point)
+  return value
+end
+
+def reduce_to_all_true(array)
+  value = reduce_to_all_true(array)
+  return value
+end
+
+def reduce_to_any_true(array)
+  value = reduce_to_any_true(array)
+  return value
+end
